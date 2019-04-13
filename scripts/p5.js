16655c6f24f5b3b5621d113694b8fa5be53c184b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-04-13 14:49:05 -0700

    interativity test

--- scripts/p5.js
+++ scripts/p5.js
@@ -1,5 +1,13 @@
-function setup() {}
+function setup() {
+  createCanvas(640, 480);
+}
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  clear();
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
